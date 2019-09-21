b8ac7a1720723a4d938ecd59da527c64aa3d5165
mattno312 <mattno312@gmail.com>
2019-09-21 07:25:33 -0500

    fuck

--- src/App.js
+++ src/App.js
@@ -1,26 +1,134 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Play from "./asset/play2.mp4";
 
-function App() {
+class App extends Component {
+  render() {
+    return (
+      <div className="App" ref="app">
+        <Header />
+      </div>
+    );
+  }
+}
+
+const BgVid = props => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-vid">
+      <video src={props.src} autoPlay loop muted />
     </div>
   );
-}
+};
+
+const Header = props => {
+  return (
+    <header className="header">
+      <BgVid src={Play} />
+
+      <div className="inner">
+        <h1>
+          Premium Domain Names for Sale!
+          <h2>
+            <div className="god">
+              prayforhealth.com
+              <br />
+              godisworld.com
+              <br />
+              praytheway.com
+              <br />
+              guaranteedjoy.com
+              <br />
+              lovegodtoday.com
+              <br />
+              connectingwithchrist.com
+              <br />
+              theanswering.com
+            </div>
+            <div className="phone">
+              fitliquids.com
+              <br />
+              cellhealer.com
+              <br />
+              mobileliquids.com
+              <br />
+              flexiblewearables.com
+              <br />
+              micromobilitymedical.com
+              <br />
+              micromobilityservices.com
+              <br />
+              micromobilitysolutions.com
+            </div>
+            <div className="money">
+            liquormoney.com
+            <br />
+            liquidcashloan.com
+            <br />
+            wecaremoney.com
+            </div>
+            <div className="health">
+            organichealth101.com
+            <br />
+            organichealthdiet.com
+            </div>
+            <div className="meat">
+            actualmeat.com
+            <br />
+            actualmeats.com
+            </div>
+            <div className="misc">
+            proveanything.com
+            <br />
+            secrethobbies.com
+            <br />
+            amillionpossibilities.com
+            <br />
+            achievegreateness.com
+            <br />
+            duringwhich.com
+            <br />
+            unrealhotdeals.com
+            </div>
+            <div className="law">
+              legitattorneys.com
+              <br />
+              illinoisdruglawyers.com
+              <br />
+              illinoisdruglawsuit.com
+              <br />
+              illinoisdruglaw.com
+              <br />
+              illinoisdrugaddiction.com
+            </div>
+            <div className="weed">
+              illinoiscannabisresearch.com
+              <br />
+              newagecaredispensary.com
+              <br />
+              chicagoweedsale.com
+              <br />
+              chicagoweedsupply.com
+              <br />
+              chicagoweedconnect.com
+              <br />
+              getchicagolit.com
+            </div>
+            <div className="porn">
+              innocentsluts.com
+              <br />
+              slapthatpussy.com
+            </div>
+            <div className="cgi">chicagoguitarinstructor.com</div>
+            <div className="vg">
+              nomansskybeyond.com
+              <br />
+              nomansskyvr.com
+            </div>
+          </h2>
+        </h1>
+      </div>
+    </header>
+  );
+};
 
 export default App;
