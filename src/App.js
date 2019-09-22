c0803b7dec53f8d40ab87affaace3e28957eef61
mattno312 <mattno312@gmail.com>
2019-09-22 13:28:58 -0500

    formatting

--- src/App.js
+++ src/App.js
@@ -7,6 +7,16 @@ class App extends Component {
     return (
       <div className="App" ref="app">
         <Header />
+        <div className="contact">
+          Choose the domain(s) you'd like to purchase
+          <br />
+          {"&"}
+          <br />
+          Make me an offer:
+          <br />
+          <span className="fancy">mnolan312@gmail.com</span>
+          <br />
+        </div>
       </div>
     );
   }
@@ -20,115 +30,126 @@ const BgVid = props => {
   );
 };
 
-const Header = props => {
+const Header = () => {
   return (
-    <header className="header">
+    <div className="header">
       <BgVid src={Play} />
-
       <div className="inner">
-        <h1>
-          Premium Domain Names for Sale!
-          <h2>
-            <div className="god">
-              prayforhealth.com
-              <br />
-              godisworld.com
-              <br />
-              praytheway.com
-              <br />
-              guaranteedjoy.com
-              <br />
-              lovegodtoday.com
-              <br />
-              connectingwithchrist.com
-              <br />
-              theanswering.com
+        <h1>Premium Domain Names for Sale!</h1>
+        <h2>
+          <div className="wrapper">
+            <div className="row">
+              <div className="columns">
+                <div>
+                  godisworld.com
+                  <br />
+                  praytheway.com
+                  <br />
+                  prayforhealth.com
+                  <br />
+                  connectingwithchrist.com
+                  <br />
+                  lovegodtoday.com
+                  <br /><br />
+                  theanswering.com
+                  <br />
+                  guaranteedjoy.com
+                  
+                  <br />
+                  <br />
+                  cellhealer.com
+                  <br />
+                  mobileliquids.com
+                  <br />
+                  flexiblewearables.com
+                  <br />
+                  <br />
+                  micromobilitymedical.com
+                  <br />
+                  micromobilityservices.com
+                  <br />
+                  micromobilitysolutions.com
+                  <br />
+                  <br />
+                  nomansskybeyond.com
+                  <br />
+                  nomansskyvr.com
+                </div>
+              </div>
+              <div className="columns">
+                <div>
+                             
+                  illinoisdruglaw.com
+                  <br />
+                  illinoisdruglawyers.com
+                  <br />
+                  illinoisdruglawsuit.com
+                  <br />
+                  illinoisdrugaddiction.com
+                  <br />
+                  <br />
+                  legitattorneys.com<br /><br />
+                  illinoiscannabisresearch.com
+                  <br />
+                  chicagoweedsupply.com
+                  <br />
+                  chicagoweedconnect.com
+                  <br />
+                  chicagoweedsale.com
+                  <br />
+                  getchicagolit.com
+                  <br />
+                  newagecaredispensary.com
+                  <br />
+                  <br />
+                  chicagoguitarinstructor.com
+                </div>
+              </div>
+              <div className="columns">
+                <div>
+                  unrealhotdeals.com
+                  <br />
+                  liquidcashloan.com
+                  <br />
+                  wecaremoney.com
+                  <br />
+                  liquormoney.com
+                  <br />
+                  <br />
+                  amillionpossibilities.com
+                  <br />
+                  achievegreateness.com
+                  <br />
+                  proveanything.com
+                  <br />
+                  <br />
+                  organichealthdiet.com
+                  <br />
+                  organichealth101.com
+                  <br />
+                  fitliquids.com
+                  <br />
+                  <br />
+                  secrethobbies.com
+                  <br /><br />
+                  slapthatpussy.com
+                  <br />
+                  innocentsluts.com
+                  <br />
+                  <br />
+                  actualmeat.com
+                  <br />
+                  actualmeats.com
+                  <br />
+                  duringwhich.com
+                  <br />
+                </div>
+              </div>
             </div>
-            <div className="phone">
-              fitliquids.com
-              <br />
-              cellhealer.com
-              <br />
-              mobileliquids.com
-              <br />
-              flexiblewearables.com
-              <br />
-              micromobilitymedical.com
-              <br />
-              micromobilityservices.com
-              <br />
-              micromobilitysolutions.com
-            </div>
-            <div className="money">
-            liquormoney.com
-            <br />
-            liquidcashloan.com
-            <br />
-            wecaremoney.com
-            </div>
-            <div className="health">
-            organichealth101.com
-            <br />
-            organichealthdiet.com
-            </div>
-            <div className="meat">
-            actualmeat.com
-            <br />
-            actualmeats.com
-            </div>
-            <div className="misc">
-            proveanything.com
-            <br />
-            secrethobbies.com
-            <br />
-            amillionpossibilities.com
-            <br />
-            achievegreateness.com
-            <br />
-            duringwhich.com
-            <br />
-            unrealhotdeals.com
-            </div>
-            <div className="law">
-              legitattorneys.com
-              <br />
-              illinoisdruglawyers.com
-              <br />
-              illinoisdruglawsuit.com
-              <br />
-              illinoisdruglaw.com
-              <br />
-              illinoisdrugaddiction.com
-            </div>
-            <div className="weed">
-              illinoiscannabisresearch.com
-              <br />
-              newagecaredispensary.com
-              <br />
-              chicagoweedsale.com
-              <br />
-              chicagoweedsupply.com
-              <br />
-              chicagoweedconnect.com
-              <br />
-              getchicagolit.com
-            </div>
-            <div className="porn">
-              innocentsluts.com
-              <br />
-              slapthatpussy.com
-            </div>
-            <div className="cgi">chicagoguitarinstructor.com</div>
-            <div className="vg">
-              nomansskybeyond.com
-              <br />
-              nomansskyvr.com
-            </div>
-            <div className="contact">mnolan312@gmail.com</div>
-          </h2>
-        </h1>
+          </div>
+        </h2>
       </div>
-    </header>
+    </div>
   );
 };
 
